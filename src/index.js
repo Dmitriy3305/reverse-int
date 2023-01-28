abb7abebdc5f2d371d7b4810a72789841950e0c9
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-01-29 00:54:06 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  console.log(n);
+  let arr = Array.from(String(n), String).reverse();
+  for (let num of arr) {
+    if (arr[0] == '0') {
+        arr.splice(0, 1);
+    } else if (num == '-') {
+        arr.splice(-1, 1);
+    }
+  }
+  return arr.toString().replace(/[^0-9]/g, '');
 }
